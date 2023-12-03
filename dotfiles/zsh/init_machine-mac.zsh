5b7d3a3fa32331021fa4fde775c8571ea453c90a
will <88556285+willdhorn@users.noreply.github.com>
2023-12-02 19:35:35 -0500

    sync brew packages function

--- dotfiles/zsh/init_machine-mac.zsh
+++ dotfiles/zsh/init_machine-mac.zsh
@@ -16,38 +16,39 @@ function _install-brew() {
   eval "$(/opt/homebrew/bin/brew shellenv)"
   wdh-write-flag $FLAG_BREW_INSTALLED
 }
-function wdh_install-brew-shelltools() {
+function wdh-install-brew-shelltools() {
   brew bundle --file="$ZSHDIR/brewfiles/shelltools.brewfile"
   wdh-write-flag $FLAG_BREW_SHELLTOOLS
 }
-function wdh_install-brew-devtools() {
+function wdh-install-brew-devtools() {
   brew bundle --file="$ZSHDIR/brewfiles/devtools.brewfile"
   wdh-write-flag $FLAG_BREW_DEVTOOLS
 }
-function wdh_install-brew-macapps() {
+function wdh-install-brew-macapps() {
   brew bundle --file="$ZSHDIR/brewfiles/macapps.brewfile"
   if is-silicon-mac; then
     brew bundle --file="$ZSHDIR/brewfiles/macapps-silicon.brewfile"
   fi
   wdh-write-flag $FLAG_BREW_MACAPPS
 }
-
-function wdh_install-clt() {
-  xcode-select --install
-  wdh-write-flag $FLAG_CLT_INSTALLED
+function wdh-sync-brew() {
+  wdh-install-brew-shelltools
+  wdh-install-brew-devtools
+  has-flag $FLAG_BREW_MACAPPS wdh-install-brew-macapps
 }
+
 # set the login shell to the brew version of zsh
 function _set-login-shell() {
   local brewzsh="$(brew --prefix)/bin/zsh"
   if [[ $(which zsh) == "$brewzsh" ]]; then
-    # already set
+    echo "$brewzsh is already the login shell"
     return 0
   fi
   if no-has-file "$brewzsh"; then
     echo "Unable to set login shell to homebrew version of zsh: $brewzsh does not exist"
     return 1
   fi
-  echo "Setting login shell to brew version of zsh"
+  echo "Setting login shell to $brewzsh"
   echo "This may ask for your password"
   sudo chsh -s $brewzsh $USER
   echo "Login shell is now $brewzsh"
@@ -59,4 +60,8 @@ function _install-fonts() {
   tar xf $HOME/.fonts/CascadiaCode.tgz --directory=$HOME/Library/Fonts
   tar xf $HOME/.fonts/MesloLGS_NF.tgz --directory=$HOME/Library/Fonts
   wdh-write-flag $FLAG_FONTS_INSTALLED
+}
+function wdh-install-clt() {
+  xcode-select --install
+  wdh-write-flag $FLAG_CLT_INSTALLED
 }
