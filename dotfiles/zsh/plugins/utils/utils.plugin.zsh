6489aef9a8929bac2cfb7c06afded654d701184c
will <88556285+willdhorn@users.noreply.github.com>
2023-12-28 17:18:20 -0500

    refactor to fix history opts not being set

--- dotfiles/zsh/plugins/utils/utils.plugin.zsh
+++ dotfiles/zsh/plugins/utils/utils.plugin.zsh
@@ -4,8 +4,6 @@
 # but it seems to be the structure of every other plugin
 # I mean this file, not the evals.
 # This is a mess.
-eval $(cat $ZSHDIR/plugins/utils/editor.zsh)
-eval $(cat $ZSHDIR/plugins/utils/history.zsh)
-eval $(cat $ZSHDIR/plugins/utils/completion.zsh)
-
-
+eval $(cat $ZSHDIR/plugins/utils/opts/editor.zsh)
+eval $(cat $ZSHDIR/plugins/utils/opts/history.zsh)
+eval $(cat $ZSHDIR/plugins/utils/opts/completion.zsh)
