6b874d620632301a0d351f8f3de4a23c83183746
will <88556285+willdhorn@users.noreply.github.com>
2024-05-20 11:58:41 -0400

    adding mkdir and ncu aliases

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -16,6 +16,16 @@ alias edb='edgedb'
 alias sync-brew='wdh-sync-brew'
 alias zgload='zgenom reset && load-zgenom'
 
+# general
+# ls
 alias ll='ls -lh'
 alias lht='ls -lht'
 alias la='ls -A'
+# mkdir
+alias mkdir='mkdir -pv'
+function mcd() {
+  command mkdir -pv "$1" && cd "$1"
+}
+
+# node
+alias ncu="npx npm-check-updates"
